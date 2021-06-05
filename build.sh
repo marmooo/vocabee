ef666e3a833c83e99a98420cd94d97295bafed65
marmooo <marmooo@users.noreply.github.com>
2021-06-05 17:22:13 +0900

    fix minify

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ dir=docs
 cp -r src/* $dir
 minify src/index.html > $dir/index.html
 minify src/index.js > $dir/index.js
+minify src/drill/drill.js > $dir/drill/drill.js
+minify src/drill/index.html > $dir/drill/index.html
 minify src/sw.js > $dir/sw.js
 
