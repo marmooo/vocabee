da00f813ade3d31639d217ca5a0cf13bdaaaecf4
marmooo <marmooo@users.noreply.github.com>
2021-08-28 10:46:30 +0900

    from Node to Deno

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # Vocabee
+
 [英単語を効率的に学ぶアプリ](https://marmooo.github.io/vocabee/)です。
 
 ## Installation
+
 install [marmooo/mGSL](https://github.com/marmooo/mgsl)
 
 ## Build
+
 ```
 node build.js
 bash build.sh
 ```
 
 ## Attribution
-- [marmooo/mGSL](https://github.com/marmooo/mgsl) is licensed under the [CC-BY-SA 4.0](http://creativecommons.org/licenses/by-sa/4.0/).
 
+- [marmooo/mGSL](https://github.com/marmooo/mgsl) is licensed under the
+  [CC-BY-SA 4.0](http://creativecommons.org/licenses/by-sa/4.0/).
--- build.js
+++ build.js
@@ -1,25 +1,36 @@
-const fs = require('fs');
-const readEachLineSync = require('read-each-line-sync');
+import { readLines } from "https://deno.land/std/io/mod.ts";
 
-
-function generateProblems() {
-  const levelArray = [200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2200, 2600, 3000];
-  for (let i=0; i<37; i++) {
+async function generateProblems() {
+  const levelArray = [
+    200,
+    400,
+    600,
+    800,
+    1000,
+    1200,
+    1400,
+    1600,
+    1800,
+    2200,
+    2600,
+    3000,
+  ];
+  for (let i = 0; i < 37; i++) {
     levelArray.push(4000 + i * 1000);
   }
   let i = 0;
   let count = 0;
   let data = [];
-  readEachLineSync('mGSL/dist/mGSL.lst', 'utf8', (line) => {
+  const fileReader = await Deno.open("mGSL/dist/mGSL.lst");
+  for await (const line of readLines(fileReader)) {
     count += 1;
-    data.push(line.split('\t', 2).join('\t'));
+    data.push(line.split("\t", 2).join("\t"));
     if (levelArray[i] == count) {
-      fs.writeFileSync('src/data/' + levelArray[i] + '.tsv', data.join('\n'));
+      Deno.writeTextFile("src/data/" + levelArray[i] + ".tsv", data.join("\n"));
       i += 1;
       data = [];
     }
-  });
+  }
 }
 
-generateProblems();
-
+await generateProblems();
