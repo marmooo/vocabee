dac33d84e50ee8171950759577ea468beb512b67
marmooo <marmooo@users.noreply.github.com>
2024-03-30 23:37:30 +0900

    cleanup

--- src/drill.js
+++ src/drill.js
@@ -1,4 +1,7 @@
-import { Carousel, Modal } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/+esm";
+import {
+  Carousel,
+  Modal,
+} from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/+esm";
 
 const CLIENT_ID =
   "945330460050-osmelc2uen8vhdesa6kd55vvjivkm5vs.apps.googleusercontent.com";
